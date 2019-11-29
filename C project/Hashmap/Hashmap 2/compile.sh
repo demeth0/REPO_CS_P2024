86ac73a6b5068f72edd14c1d3c67e1849f3c11ba
samdec666 <samdec66@gmail.com>
2019-11-29 09:22:04 +0000

    update on hashmap project

--- C project/Hashmap/Hashmap 2/compile.sh
+++ C project/Hashmap/Hashmap 2/compile.sh
@@ -1,11 +1,37 @@
-#!/bin/bash
+
 echo "start compiling"
 gcc -o bin/Main src/Main.c 2>error.txt
-if [ "$1" == "-e" ] ; then
+
+execute_project(){
 	echo "running program:"
 	echo "--------------------------------------------------------------------------------"
 	./bin/Main
 	echo
 	echo "--------------------------------------------------------------------------------"
-fi
+}
+
+update_git(){
+	git commit -a -m "update on hashmap project"
+	git push
+}
+#if [ "$1" == "-e" ] ; then
+#	execute_project();
+#fi
+
+do_all=1
+do_git=0
+do_execute=0
+while getopts "eg" opt
+do
+    case $opt in
+    (g) do_all=0 ; do_git=1 ;;
+    (e) do_all=0 ; do_exxecute=1 ;;
+    (*) printf "Illegal option '-%s'\n" "$opt" && exit 1 ;;
+    esac
+done
+
+(( do_all || do_git )) && update_git;
+
+(( do_all || do_execute )) && execute_project;
+
 echo "compilation finished"
